c90a91b2627d62fb4efed75a8d49bab640ab4ec2
20002196 <jormun.zhang@universalbeijing.com>
2022-06-19 10:22:24 +0800

    add metrics

--- 1_metrics/2_thanos/init_thanos.sh
+++ 1_metrics/2_thanos/init_thanos.sh
@@ -3,3 +3,15 @@ wget https://github.com/thanos-io/thanos/releases/download/v0.26.0/thanos-0.26.0
 tar xf https://github.com/thanos-io/thanos/releases/download/v0.26.0/thanos-0.26.0.linux-amd64.tar.gz
 cd thanos-0.26.0.linux-amd64
 mv thanos /usr/local/sbin
+
+wget https://raw.githubusercontent.com/cloudnative365/keynotes_L5_architect_observability/main/1_metrics/2_thanos/thanos-sidecar.service
+wget https://raw.githubusercontent.com/cloudnative365/keynotes_L5_architect_observability/main/1_metrics/2_thanos/thanos-query.service
+
+mv thanos-sidecar.service /etc/systemd/system/
+mv thanos-query.service /etc/systemd/system/
+
+systemctl start thanos-sidecar
+systemctl start thanos-query
+
+systemctl enable thanos-sidecar
+systemctl enable thanos-query
